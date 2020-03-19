66e0e6c8dd5f208783cf29877bb9b58e875317e9
Akarachai Pannoi <ballboo088@Akarachais-MacBook-Pro-2.local>
2020-03-19 18:09:51 +0700

    first

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyClIF27ZIvlBNs2ym_JnHqSKLSmRFSrT-8",
+    authDomain: "todo-242311.firebaseapp.com",
+    databaseURL: "https://todo-242311.firebaseio.com",
+    projectId: "todo-242311",
+    storageBucket: "todo-242311.appspot.com",
+    messagingSenderId: "989993008957",
+    appId: "1:989993008957:web:cb88ac4c2d43f1f7778f54",
+    measurementId: "G-644K2HKS41"
+  };
+export default config 
